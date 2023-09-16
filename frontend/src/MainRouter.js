c009820d2efb33de1c59e1b9dc0c513b52a1cf18
spencerckhuang <shuan126@jh.edu>
2023-09-16 10:22:25 -0400

    frontend file restructuring

---
+++ frontend/src/MainRouter.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Route, Routes} from "react-router-dom";
+// import Home from './pages/PatientHome';
+import pages from './pages/pages.js';
+
+const tempComponent = () => {
+    return (
+        <h1>Hello World</h1>
+    )
+}
+
+function MainRouter() {
+
+    return (
+        <div>
+            <Routes>
+                <Route path={'/'} element={tempComponent}/>
+                <Route path={'new/'} element={pages.SignUp}/>
+                <Route path={'patientHome/'} element={pages.PatientHome}/>
+                <Route path={'researcherHome/'} element={pages.ResearcherHome}/>
+            </Routes>
+        </div>
+    )
+}
+
+export default MainRouter
---
+++ frontend/src/pages/pages.js
@@ -0,0 +1,11 @@
+import PatientHome from "./PatientHome"
+import ResearcherHome from "./ResearcherHome"
+import SignUp from "./SignUp"
+
+const pages = {
+    PatientHome,
+    ResearcherHome,
+    SignUp
+}
+
+export default pages
