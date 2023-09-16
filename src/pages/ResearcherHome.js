585ca7a7d67a9dccfdef014afd499092eb26d1e3
spencerckhuang <shuan126@jh.edu>
2023-09-16 10:16:37 -0400

    frontend: making pages, working on initial routing

---
+++ src/pages/ResearcherHome.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function ResearcherHome(){
+
+    return (
+        <div>
+            <h1>This is the researcher home</h1>
+        </div>
+    )
+}
+export default ResearcherHome
---
+++ src/pages/SignUp.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function SignUp(){
+
+    return (
+        <div>
+            <h1>Hola</h1>
+        </div>
+    )
+}
+export default SignUp
