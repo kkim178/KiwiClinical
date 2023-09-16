39355a21bdef5fb3450da6d3e017658e6460416e
Nicolas Liu <nicolasliu18@gmail.com>
2023-09-16 10:40:54 -0400

    index

--- frontend/src/MainRouter.js
+++ frontend/src/MainRouter.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Route, Routes} from "react-router-dom";
 // import Home from './pages/PatientHome';
 import pages from './pages/pages.js';
+import Index from './pages/Index.js';
 
 const tempComponent = () => {
     return (
@@ -14,7 +15,7 @@ function MainRouter() {
     return (
         <div>
             <Routes>
-                <Route path={'/'} element={tempComponent}/>
+                <Route path={'/'} element={<Index/>}/>
                 <Route path={'new/'} element={pages.SignUp}/>
                 <Route path={'patientHome/'} element={pages.PatientHome}/>
                 <Route path={'researcherHome/'} element={pages.ResearcherHome}/>
---
+++ frontend/src/pages/Index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function Index(){
+    console.log("Made it to patient home component")
+
+    return (
+        <div>
+            <h1>WELCOME TO KIWI</h1>
+        </div>
+    )
+}
+export default Index
